3dff93b881b748d5cb0271fdd93ac28cecce60e0
Anabil Debnath <73880095+anabildebnath@users.noreply.github.com>
2024-05-06 07:24:31 +0600

    json force install

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,19 +6,29 @@ import "boxicons";
 export default function Nav() {
   return (
     <nav className={classes.nav}>
-      <ul className={classes.alignment}>
-        <li>
-          <Link to="/" className={classes.brand}>
-            <box-icon
-              type="solid"
-              name="bar-chart-alt-2"
-              color="white"
-            ></box-icon>
-            <h1>SweQuizer</h1>
-          </Link>
-        </li>
-      </ul>
-      <Account />
+      <div className={classes.brand}>
+        <ul className={classes.alignment}>
+          <li>
+            <Link to="/" className={classes.brand}>
+              <box-icon
+                type="solid"
+                name="bar-chart-alt-2"
+                color="white"
+              ></box-icon>
+              <h1>SweQuizer</h1>
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <div className={classes.navChild}>
+        <ul>
+          <li>Upcoming Tests</li>
+          <li>Tutorials</li>
+          <li>Results</li>
+          <li>Contact Faculty</li>
+          <Account />
+        </ul>
+      </div>
     </nav>
   );
 }
